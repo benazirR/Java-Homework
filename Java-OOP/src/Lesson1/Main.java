6f3e04a7d0082fdaaf08f4f5d326b7b235d077c8
B_R <b.rajabov@eskhata.tj>
2024-06-24 13:54:22 +0500

    add changes

--- Java-OOP/src/Lesson1/Main.java
+++ Java-OOP/src/Lesson1/Main.java
@@ -57,5 +57,7 @@ public class Main {
 
         System.out.println("--------------------");
         System.out.println(HDvm.getProducts());
+
+        System.out.println("--------------------");
     }
 }
